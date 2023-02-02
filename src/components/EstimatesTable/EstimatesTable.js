7c377415cce4b33ac412f361edf8c23e2b2d6f48
Daniel Orozco <00200617@uca.edu.sv>
2023-02-02 10:40:56 -0600

    Fix: Formatting for multiple rows in EstimateTable

--- src/components/EstimatesTable/EstimatesTable.js
+++ src/components/EstimatesTable/EstimatesTable.js
@@ -10,10 +10,14 @@ import { ExportCSV } from "../ExportCSV/ExportCSV";
 export const EstimatesTable = ({ estimates }) => {
 
   const tableHeader = {background:"#303845", color:"white", fontWeight: "900"};
-  console.log(estimates);
-  for(let i = 0; i < estimates.length; i++){
-    estimates[i].quantity = numeral(estimates[i].quantity).format('0,0');
+  console.log("estimates table",JSON.stringify(estimates))
+
+  if(estimates){
+    estimates.forEach(estimate => {
+      estimate.cantidad_de_consumo = numeral(estimate.cantidad_de_consumo).format('0,0');
+    })
   }
+  // estimates[0].quantity = numeral(estimates[0].quantity).format('0,0');
   return (
     <div className="containerTable">
         <Table
@@ -29,22 +33,22 @@ export const EstimatesTable = ({ estimates }) => {
             <Table.HeaderCell style={tableHeader}>
               Identificacion de cuenca
             </Table.HeaderCell>
-            <Table.Cell dataKey="cuencaId" />
+            <Table.Cell dataKey="id_cuenca" />
           </Table.Column>
           <Table.Column align="center" width={250}>
             <Table.HeaderCell style={tableHeader}>Consumo</Table.HeaderCell>
-            <Table.Cell dataKey="type" />
+            <Table.Cell dataKey="tipo_de_consumo" />
           </Table.Column>
           
             <Table.Column width={250} align="center" fixed>
               <Table.HeaderCell style={tableHeader}>Cantidad</Table.HeaderCell>
-              <Table.Cell dataKey="quantity" />
+              <Table.Cell dataKey="cantidad_de_consumo" />
             </Table.Column>
           
 
           <Table.Column align="center" width={250}>
             <Table.HeaderCell style={tableHeader}>Indice</Table.HeaderCell>
-            <Table.Cell dataKey="explotationindex" />
+            <Table.Cell dataKey="indice_de_explotacion" />
           </Table.Column>
 
           <Table.Column width={250} align="center" fixed="right">
